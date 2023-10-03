e82aa42096a49faad5e853a94dbca3e903b510ae
yappa <julijaogr@ukr.net>
2023-10-03 13:18:48 +0300

    made responsive slider

--- src/widgets/Slider/model/index.ts
+++ src/widgets/Slider/model/index.ts
@@ -17,8 +17,8 @@ export const sliderItems: ISliderItem[] = [
     },
     {
         imageSrc: require("../../../assets/slider/slider-image-2.png"),
-        title: "2022 під час воєнного стану",
-        text: "Вступна кампанія",
+        title: "Начальник інформаційно-телекомунікаційного вузла",
+        text: "Військова професія",
         descriptionButton: {
             href: "/",
             text: "ДЕТАЛЬНІШЕ"
