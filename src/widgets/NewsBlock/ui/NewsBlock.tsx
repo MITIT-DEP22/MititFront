689496e59d940f3ed98ca16c4899441f7ab2131d
vladlenmarchenko <markelovwtf7@gmail.com>
2023-10-16 12:36:56 +0300

    (fix) changed news Store

--- src/widgets/NewsBlock/ui/NewsBlock.tsx
+++ src/widgets/NewsBlock/ui/NewsBlock.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import BlockContainer from "shared/ui/BlockContainer/BlockContainer";
+import {faculties} from "shared/enums/Data";
 
 export const NewsBlock = () => {
     return (
         <BlockContainer>
             NewsBlock
+            <button onClick={() => {
+                console.log(JSON.stringify(faculties[0]))
+            }}>asd</button>
         </BlockContainer>
     );
 };
