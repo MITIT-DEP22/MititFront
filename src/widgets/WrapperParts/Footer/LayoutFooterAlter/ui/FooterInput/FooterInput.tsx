08222136f510035c38c4fdffc617ea1d9ffbd212
Vladislav <fletspleyhik@gmail.com>
2023-10-16 16:07:13 +0300

    (fix) footer

--- src/widgets/WrapperParts/Footer/LayoutFooterAlter/ui/FooterInput/FooterInput.tsx
+++ src/widgets/WrapperParts/Footer/LayoutFooterAlter/ui/FooterInput/FooterInput.tsx
@@ -4,8 +4,8 @@ import css from "./FooterInput.module.scss"
 const FooterInput: FC = () => {
     return (
         <div className={css.inputContainer}>
-            <input type="email" className={css.inputMain} placeholder="Email" name="email" id='email' required/>
-            <label className={css.inputLabel}>Email</label>
+            <input type="email" className={css.inputMain} placeholder="Email" name="email" id='email' required />
+            <label htmlFor="email" className={css.inputLabel}>Email</label>
         </div>
     );
 };
