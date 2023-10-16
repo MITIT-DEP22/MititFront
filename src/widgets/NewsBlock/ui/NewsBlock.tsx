0c4d1de46361535fdd4e52617f3994963fa2cb7d
vladlenmarchenko <markelovwtf7@gmail.com>
2023-10-16 12:37:06 +0300

    (fix) changed news Store

--- src/widgets/NewsBlock/ui/NewsBlock.tsx
+++ src/widgets/NewsBlock/ui/NewsBlock.tsx
@@ -5,10 +5,7 @@ import {faculties} from "shared/enums/Data";
 export const NewsBlock = () => {
     return (
         <BlockContainer>
-            NewsBlock
-            <button onClick={() => {
-                console.log(JSON.stringify(faculties[0]))
-            }}>asd</button>
+
         </BlockContainer>
     );
 };
