2cbd2da4e6f600e63819840482d4d8124f487271
Vladislav <fletspleyhik@gmail.com>
2023-10-16 15:42:26 +0300

    (feature) Science Page

--- src/widgets/ScienceBlock/ui/ScienceBlock.tsx
+++ src/widgets/ScienceBlock/ui/ScienceBlock.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import BlockContainer from "shared/ui/BlockContainer/BlockContainer";
+import SectionTitle from "../../../shared/ui/SectionTitle/SectionTitle";
+import {sciences} from "../../../shared/enums/Data";
+import InfoAccordion from "../../../shared/ui/InfoAccordion/InfoAccordion";
 
 export const ScienceBlock = () => {
     return (
         <BlockContainer>
-            ScienceBlock
+            <SectionTitle title={"Наука"}/>
+            {sciences.map(science => (
+                <InfoAccordion entrance={science}/>
+            ))}
         </BlockContainer>
     );
 };
