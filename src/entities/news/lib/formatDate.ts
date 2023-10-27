23c40ccf16c58a7e3c0ef3c0b5c4190ff8a0138c
Vladlen <88963754+VladLennin@users.noreply.github.com>
2023-10-27 15:30:10 +0300

    fixed news date  parse

--- src/entities/news/lib/formatDate.ts
+++ src/entities/news/lib/formatDate.ts
@@ -1,11 +1,14 @@
 import {Month} from "shared/enums/Month";
 
-export function formatDate(date: string) {
-    if (date) {
-        let temp = date?.split('-')
-        return `${temp[2].split("T")[0]} ${Month[Number(temp[1])]} ${temp[0]}`
-    } else {
-        return ""
+export function formatDate(date: any) {
+    try {
+        if (typeof date === "string") {
+            let temp = date?.split('-')
+            return `${temp[2].split("T")[0]} ${Month[Number(temp[1])]} ${temp[0]}`
+        } else {
+            return ""
+        }
+    } catch (e) {
+        console.log(e)
     }
-
 }
