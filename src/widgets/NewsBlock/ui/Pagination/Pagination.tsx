a04c451c9914a033a54f1325ed9e277c01d2f540
Vladlen <88963754+VladLennin@users.noreply.github.com>
2023-10-30 09:18:07 +0200

    (fix) changed pagination on NewsPage

--- src/widgets/NewsBlock/ui/Pagination/Pagination.tsx
+++ src/widgets/NewsBlock/ui/Pagination/Pagination.tsx
@@ -2,49 +2,10 @@ import React from 'react';
 import css from "./Pagination.module.scss"
 import newsStore from "../../../../entities/news/store/news-page-store";
 import {observer} from "mobx-react";
-import {observe} from "mobx";
 
 const Pagination = observer(() => {
 
     const {countPages, page} = newsStore
-
-    // const renderPageNumbers = () => {
-    //     const pageNumbers = [];
-    //
-    //     if (countPages <= 5) {
-    //         for (let i = 0; i < countPages; i++) {
-    //             pageNumbers.push(i+1);
-    //         }
-    //     } else {
-    //         if (page <= 2) {
-    //             pageNumbers.push(1, 2, 3, '...', countPages);
-    //         } else if (page >= countPages - 1) {
-    //             pageNumbers.push(1, '...', countPages - 2, countPages - 1, countPages);
-    //         } else {
-    //             pageNumbers.push(1, '...', page - 1, page, page + 1, '...', countPages);
-    //         }
-    //     }
-    //
-    //     return pageNumbers.map((pageNumber, index) => (
-    //         pageNumber === '...' ?
-    //             <span className={css.separator}>{pageNumber}</span>
-    //             :
-    //             <button
-    //                 key={index}
-    //                 className={`${css.paginationBtn} ${Number(pageNumber)-1 === page && css.paginationBtn_active}`}
-    //                 onClick={() => newsStore.changePage(Number(pageNumber) -1)}>
-    //                 {pageNumber}
-    //             </button>
-    //     ));
-    // };
-    //
-    // return (
-    //     <div className={css.container}>
-    //         {renderPageNumbers()}
-    //     </div>
-    // );
-
-    const pagesToShow = 1; // Number of pages to show between '...' on each side of the active page
 
     const handlePageChange = (page: number) => {
         if (page >= 0 && page <= countPages) {
@@ -53,26 +14,31 @@ const Pagination = observer(() => {
     };
 
     const renderPageNumbers = () => {
+        const pagesToShow = 5;
         const pageNumbers = [];
-        let startPage = Math.max(0, page - pagesToShow);
-        let endPage = Math.min(countPages, page + pagesToShow);
 
-        if (page - pagesToShow > 1) {
-            pageNumbers.push(0);
-            if (page - pagesToShow > 2) {
-                pageNumbers.push('...');
+        const addPageNumber = (pageNumber:any) => {
+            if (pageNumber >= 0 && pageNumber < countPages) {
+                pageNumbers.push(pageNumber);
             }
+        };
+
+        addPageNumber(0);
+
+        if (page > 3) {
+            pageNumbers.push("...");
         }
 
-        for (let i = startPage; i <= endPage-1; i++) {
-            pageNumbers.push(i);
+        for (let i = Math.max(1, page - 2); i <= Math.min(countPages - 1, page + 2); i++) {
+            addPageNumber(i);
         }
 
-        if (page + pagesToShow < countPages) {
-            if (page + pagesToShow < countPages - 1) {
-                pageNumbers.push('...');
-            }
-            pageNumbers.push(countPages);
+        if (page < countPages - pagesToShow) {
+            pageNumbers.push("...");
+        }
+
+        if (page !== countPages - 1) {
+            addPageNumber(countPages - 1);
         }
 
         return pageNumbers.map((item, index) => (
@@ -81,7 +47,7 @@ const Pagination = observer(() => {
                 className={Number(item) === page ? css.paginationBtn_active : ""}
                 onClick={() => handlePageChange(Number(item))}
             >
-                {typeof (item) === "number" ? Number(item) + 1 : item}
+                {typeof item === "number" ? item + 1 : item}
             </button>
         ));
     };
@@ -107,7 +73,3 @@ const Pagination = observer(() => {
 });
 
 export default Pagination;
-
-
-
-
