832a1104bfa6e3c42de69cb9af4205a61482ee01
VladLennin <markelovwtf7@gmail.com>
2023-11-22 11:52:04 +0200

    (fix) fixed IMAGE_PATH

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const API_URL =  process.env.REACT_APP_HOST
-export const IMAGES_PATH = API_URL + "images/"
+export const IMAGES_PATH = API_URL + "/images/"
 const $api = axios.create({
     baseURL: API_URL,
 })
